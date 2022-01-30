9ef668e3bf924988d020266758770fc8b567c864
Joe Harris <32495689+yeezus24@users.noreply.github.com>
2022-01-30 15:24:15 +0000

    made it easier to read, and commented #livelaughlove

--- domain-registration.py
+++ domain-registration.py
@@ -1,35 +1,48 @@
 import requests
 
+# will be using the rdap api to find out as much domain registration info as poss
 def rdap(domain, tld):
-	# Make two API calls using RDAP API to get domain registration details
-	# res obtains domain registrant
-	#res = requests.get("https://domainsrdap.googleapis.com/v1/domain/4MATIONDRILLING.COM")
-	# NEED TLD FOR THIS TO GO BETWEEN .COM/ AND /V1
+
+	# initialize rdap_object which will store all info related to domain registration
+	rdap_object = {"registrar": {}, "registrant": {}}
+
+	# initial api call to get registrar information using rdap api
 	res2 = requests.get("https://rdap.verisign.com/" + tld + "/v1/domain/" + domain)
 	
+	# store registration events in initial_registration
 	initial_registration = res2.json()['events']
-	#registration = res.json()
-	#print(registration)
-	
+
+	# for each of the registration events
 	for i in initial_registration:
+
 		if i['eventAction'] == 'registration':
 			reg_date = i['eventDate']
-			print(reg_date)
+			rdap_object['registrationDate'] = reg_date
+
 		elif i['eventAction'] == 'expiration':
 			exp_date = i['eventDate']
-			print(exp_date)
+			rdap_object['expirationDate'] = exp_date
+
 		elif i['eventAction'] == 'last changed':
 			upd_date = i['eventDate']
-			print(upd_date)
+			rdap_object['updateDate'] = upd_date
 
-	for i in res2.json()['entities']:
+	# iterate through entities obtained from res2 request
+	for entity in res2.json()['entities']:
 		
-		if 'registrar' in i['roles']:
-			for z in i['vcardArray']:
-				for y in z:
+		# find information about the registrar
+		if 'registrar' in entity['roles']:
+
+			# api returns a strange double array, iterate through these
+			for x in entity['vcardArray']:
+				for y in x:
+
+					# 'fn' relates to the area containing registrar details (i.e. name)
 					if 'fn' in y:
 						registrar_name = y[-1]
-						print(registrar_name)
+						rdap_object['registrar']['registrarName'] = registrar_name
 	
-	
+	return rdap_object
+
+# we're using a domain from the most recent NCSC malicious domain list
 rdap('4MATIONDRILLING.COM', 'com')
