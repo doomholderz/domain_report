753bdfd323720f9202f578140fef562cc170144b
Joe Harris <32495689+yeezus24@users.noreply.github.com>
2022-01-30 17:59:17 +0000

    added ip lookup, and commented very nice yes

--- censys-lookup.py
+++ censys-lookup.py
@@ -8,23 +8,34 @@ CENSYS_API_SECRET = ""
 API_URL = "https://search.censys.io/api"
 
 def subdomains(domain):
+	# using count for rate limiting
 	count = 0
+
+	# define the query parameter used for POST request, using domain parameter
 	params = {"query" : "parsed.names: " + domain}
 	
+	# initialise the subdomains object where all subdomain info will be stored
 	subdomains_obj = {}
 
 	# get all certificates that even smell of 'domain'
 	res = requests.post(API_URL + "/v1/search/certificates", json = params, auth=(CENSYS_API_ID, CENSYS_API_SECRET))
 	cert_json = res.json()
 	
+	# for each certificate relating to '*domain*', harvest information
 	for cert in cert_json['results']:
 		count += 1
-		if count < 10:
+		if count < 50:
+			# get the certificate name (subdomain), which we'll use as a key in this kv-pair
 			cert_name = cert['parsed.subject_dn'].split("CN=")[-1]
+			
+			# get the sha256 encoded fingerprint of the certificate (used for searching)
 			cert2 = cert['parsed.fingerprint_sha256']
+
+			# censys API request to get information about this certificate
 			res2 = requests.get(API_URL + "/v1/view/certificates/" + cert2, auth=(CENSYS_API_ID, CENSYS_API_SECRET))
 			res2j = res2.json()
-			print(str(count))
+			
+			# harvest and store additional data pertaining to the subdomain
 			common_name = res2j['parsed']['subject']['common_name']
 			issuer = res2j['parsed']['issuer']['common_name']
 			start = res2j['parsed']['validity']['start']
@@ -35,7 +46,46 @@ def subdomains(domain):
 			subdomains_obj[cert_name]['certificateEndDate'] = end
 			subdomains_obj[cert_name]['issuer'] = issuer
 			subdomains_obj[cert_name]['domainNames'] = common_name
+			subdomains_obj[cert_name]['san'] = SAN
 			
 	return subdomains_obj
 
-print(subdomains(''))
+def ips(domain):
+	# initialise the ips object, where we'll store info about related ips to domain
+	ips_obj = {}
+
+	# GET request using censys to get information about the domain
+	res = requests.get(API_URL + "/v2/hosts/search?q=" + domain + " and service.service_name='HTTP'", auth=(CENSYS_API_ID, CENSYS_API_SECRET))
+	
+	# for each ip found relating to the domain
+	for hit in res.json()['result']['hits']:
+
+		# initialise an entry in ips_obj for the ip, to store all info about it
+		ips_obj[hit['ip']] = {}
+
+		# throw in some initially useful info about the ip
+		ips_obj[hit['ip']]['services'] = hit['services']
+		ips_obj[hit['ip']]['location'] = hit['location']['country'] + " - " + hit['location']['city']
+		ips_obj[hit['ip']]['asn'] = {'asnId': hit['autonomous_system']['asn'], 'name': hit['autonomous_system']['name']}
+		
+		# now we do a further API call to get more information about the ip not initially available
+		res2 = requests.get(API_URL + "/v2/hosts/" + hit['ip'], auth=(CENSYS_API_ID, CENSYS_API_SECRET)).json()['result']['services']
+		
+		# for each of the services running on this IP (will only be HTTP/HTTPS)
+		for service in res2:
+
+			# use the extended service name (HTTP/HTTPS) as the key for the entry to ips_obj['ip']
+			ips_obj[hit['ip']][service['extended_service_name']] = {}
+
+			# if its an HTTPS service, we can get the JARM signature and the domains relating to certificate
+			if service['extended_service_name'] == "HTTPS": 
+				ips_obj[hit['ip']][service['extended_service_name']]['domains'] = service['tls']['certificates']['leaf_data']['names']
+				ips_obj[hit['ip']][service['extended_service_name']]['jarm'] = service['jarm']['fingerprint']
+			
+			# get all software censys knows is running 
+			ips_obj[hit['ip']][service['extended_service_name']]['software'] = service['software']
+			
+	return(ips_obj)
+
+subdomains_obj = subdomains('')
+ips_obj = ips('')
