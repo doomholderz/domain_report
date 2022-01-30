05973e1c3504afcbe3c14c8b494f5ff915fe3830
Joe Harris <32495689+yeezus24@users.noreply.github.com>
2022-01-30 08:55:22 +0000

    added entropy and changed return
    added in domain_enrichment_obj which will be returned at the end

--- ttp-enrichment.py
+++ ttp-enrichment.py
@@ -1,4 +1,5 @@
 from tld import get_tld
+import math
 
 def breakdown_domain(domain):
 	# i.e. breakdown of twetter.account-management.f53dw.io results in
@@ -9,7 +10,16 @@ def breakdown_domain(domain):
 	domain = {"domain": '.'.join([domain_parts.domain, domain_parts.subdomain]), "innerdomain": domain_parts.domain, "subdomains": domain_parts.subdomain, "tld": domain_parts.tld}
 	return domain
 
+def entropy(domain):
+    prob = [ float(domain.count(i)) / len(domain) for i in dict.fromkeys(list(domain))]
+    entropy = -sum([p * math.log(p) / math.log(2.0) for p in prob])
+    return entropy
+
 example_domain = "twetter.account-management.f53dw.io"
 
 domain = breakdown_domain(example_domain)
-print(domain)
+domain_entropy = entropy(domain['domain'])
+
+domain_enrichment_obj = {"domain": domain, "domainEntropy": domain_entropy}
+
+print(domain_enrichment_obj)
