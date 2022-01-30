8ae19737426127dfa87dbfee362fe2bfeb75b3d3
Joe Harris <32495689+yeezus24@users.noreply.github.com>
2022-01-30 15:37:29 +0000

    updated to have a look at registrant

--- domain-registration.py
+++ domain-registration.py
@@ -4,7 +4,7 @@ import requests
 def rdap(domain, tld):
 
 	# initialize rdap_object which will store all info related to domain registration
-	rdap_object = {"registrar": {}, "registrant": {}}
+	rdap_object = {"registrar": {}, "registrant": {"registrantName": "", "organisationName": "", "address": "", "telephone": "", "email": ""}}
 
 	# initial api call to get registrar information using rdap api
 	res2 = requests.get("https://rdap.verisign.com/" + tld + "/v1/domain/" + domain)
@@ -42,7 +42,46 @@ def rdap(domain, tld):
 						registrar_name = y[-1]
 						rdap_object['registrar']['registrarName'] = registrar_name
 	
+	# we now need to find the link to the registrant's info, so iterate through links
+	for link in res2.json()['links']:
+		# here rel = related for the link relating to the registrant
+		if link['rel'] == 'related':
+			print(link['href'])
+			registrant_link = link['href']
+
+	# we're now going to GET from the registrant's link, another RDAP API call
+	registrant_res = requests.get(registrant_link)
+	
+	# iterate through the entities to find registrant's details, and store
+	for i in registrant_res.json()['entities']:
+		if 'registrant' in i['roles']:
+
+			# weird nested array thing again
+			for z in i['vcardArray']:
+				for y in z:
+
+					if 'fn' in y:
+						customer_name = y[-1]
+						rdap_object['registrant']['registrantName'] = customer_name
+					if 'org' in y:
+						org_name = y[-1]
+						rdap_object['registrant']['organisationName'] = org_name
+					if 'adr' in y:
+						address_array = y[-1]
+						address = ""
+						for string in address_array:
+							if string != "":
+								address += string + " "
+						rdap_object['registrant']['address'] = address
+					if 'tel' in y:
+						telephone = y[-1]
+						rdap_object['registrant']['telephone'] = telephone
+					if 'email' in y:
+						email = y[-1]	
+						rdap_object['registrant']['email'] = email
+
+
 	return rdap_object
 
 # we're using a domain from the most recent NCSC malicious domain list
-rdap('4MATIONDRILLING.COM', 'com')
+print(rdap('4MATIONDRILLING.COM', 'com'))
